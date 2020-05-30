9db36434576f7556bb9f2a0c9aa7ad291ca805f3
baybilon <baybilon_iqbal@hotmail.com>
2020-05-30 20:59:48 +0700

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -10,7 +10,7 @@ import time, random, sys, json, codecs, threading, glob, re, string, os, request
 with open('token.json', 'r') as fp:
     connecting = json.load(fp)
 if connecting['token'] == "":
-     client = LINE("email","pass")
+     client = LINE("hanskimmer43@gmail.com","ihateababil88")
 else:
     client=LINE(authToken=connecting['token'])
 with open('cctv.json', 'r') as fp:
